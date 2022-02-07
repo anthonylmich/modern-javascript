572d18e8ec6f2834db999c33f40d11723c55d49d
Anthony <tonymich2012@gmail.com>
2022-02-07 12:04:55 -0800

    adds Babel and webpack config file.

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const dayjs = require("dayjs");
 
 console.log("Hello from javascript");
 console.log(dayjs().format());
+function square() {
+  let example = () => {
+    let numbers = [];
+    for (let number of arguments) {
+      numbers.push(number * number);
+    }
+
+    return numbers;
+  };
+
+  return example();
+}
+console.log(square(2, 4, 6, 8, 225, 11.5, 21.3));
+
+let name = "jimmy",
+  time = "tuesday";
+const greeting = `Hey ${name}! Hows your ${time} been?`;
+console.log(greeting);
